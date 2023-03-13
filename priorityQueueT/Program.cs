ae0b3628fab601f1b948edf938090525447af7b9
danielchoike2 <89948222+danielchoike2@users.noreply.github.com>
2023-03-13 16:27:49 -0400

    Add project files.

---
+++ priorityQueueT/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace SimpleFoodPriorityQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create the queue
+            // The data items are strings. The second type parameter (an int) indicates the priority
+            PriorityQueue<string, int> messages = new PriorityQueue<string, int>();
+
+            // Add messages with different priorities to the queue
+            messages.Enqueue("Candy", 1);
+            messages.Enqueue("Soda", 2);
+            messages.Enqueue("Steak", 3);
+            messages.Enqueue("Pizza", 4);
+            messages.Enqueue("Spagehtti", 5);
+            messages.Enqueue("Applies", 6);
+
+            //Count items in the queue
+           
+
+            // Retrieve the messages in priority and queue order
+            while (messages.TryDequeue(out string item, out int priority))
+            {
+                Console.WriteLine($"There are {messages.Count} items in the queue");
+                Console.WriteLine($"Dequeued Item : {item} Priority Was : {priority}");
+            }
+        }
+    }
+}
